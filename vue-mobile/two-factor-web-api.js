0f7c047a5f91371d596f10bf38492b9798f7d0b3
nadine-shi <nadinshi@gmail.com>
2022-02-10 14:42:01 +0300

    web API methods

--- vue-mobile/two-factor-web-api.js
+++ vue-mobile/two-factor-web-api.js
@@ -1,35 +1,33 @@
 import webApi from 'src/api/web-api'
 
-export default () => {
-  return {
-    confirmTwoFactorAuth: async (parameters) => {
-      return webApi.sendRequest({
-        moduleName: 'TwoFactorAuth',
-        methodName: 'VerifyAuthenticatorAppCode',
-        parameters,
-      }).then((result) => {
-        return result
-      })
-    },
+export default {
+  confirmTwoFactorAuth: async (parameters) => {
+    return webApi.sendRequest({
+      moduleName: 'TwoFactorAuth',
+      methodName: 'VerifyAuthenticatorAppCode',
+      parameters,
+    }).then((result) => {
+      return result
+    })
+  },
 
-    trustTheDevice: async (parameters) => {
-      return webApi.sendRequest({
-        moduleName: 'TwoFactorAuth',
-        methodName: 'TrustDevice',
-        parameters,
-      }).then((result) => {
-        return result
-      })
-    },
+  trustTheDevice: async (parameters) => {
+    return webApi.sendRequest({
+      moduleName: 'TwoFactorAuth',
+      methodName: 'TrustDevice',
+      parameters,
+    }).then((result) => {
+      return result
+    })
+  },
 
-    getUsedDevices: async (parameters) => {
-      return webApi.sendRequest({
-        moduleName: 'TwoFactorAuth',
-        methodName: 'GetUsedDevices',
-        parameters,
-      }).then((result) => {
-        return result
-      })
-    },
-  }
+  getUsedDevices: async (parameters) => {
+    return webApi.sendRequest({
+      moduleName: 'TwoFactorAuth',
+      methodName: 'GetUsedDevices',
+      parameters,
+    }).then((result) => {
+      return result
+    })
+  },
 }
