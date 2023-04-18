a50ab0f1f209358542de4bffcc163ed37fa6aebb
Vasil Sokolov <vasil@afterlogic.com>
2023-04-18 11:19:50 +0200

    Refactored module settings

--- Module.php
+++ Module.php
@@ -41,6 +41,15 @@ class Module extends \Aurora\System\Module\AbstractModule
         return parent::Decorator();
     }
 
+    /**
+     *
+     * @return Settings
+     */
+    protected function GetModuleSettings()
+    {
+        return $this->oModuleSettings;
+    }
+
     public function init()
     {
         \Aurora\System\Router::getInstance()->registerArray(
