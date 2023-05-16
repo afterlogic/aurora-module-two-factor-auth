4b9b8696f1a6372f868ca1d4cf94635ba31473e5
nadine-shi <nadinshi@gmail.com>
2023-05-16 19:39:13 +0300

    Format code

--- js/manager.js
+++ js/manager.js
@@ -1,110 +1,91 @@
-'use strict';
+'use strict'
 
-var
-	_ = require('underscore'),
+const _ = require('underscore')
 
-	TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
+const TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
+  Ajax = require('%PathToCoreWebclientModule%/js/Ajax.js'),
+  App = require('%PathToCoreWebclientModule%/js/App.js')
 
-	Ajax = require('%PathToCoreWebclientModule%/js/Ajax.js'),
-	App = require('%PathToCoreWebclientModule%/js/App.js'),
-
-	DeviceUtils = require('modules/%ModuleName%/js/utils/Device.js'),
-
-	Settings = require('modules/%ModuleName%/js/Settings.js')
-;
+const DeviceUtils = require('modules/%ModuleName%/js/utils/Device.js'),
+  Settings = require('modules/%ModuleName%/js/Settings.js')
 
 module.exports = function (oAppData) {
-	Settings.init(oAppData);
+  Settings.init(oAppData)
 
-	return {
-		/**
-		 * Runs before application start. Subscribes to the event before post displaying.
-		 * 
-		 * @param {Object} ModulesManager
-		 */
-		start: function (ModulesManager) {
-			if (!App.isMobile())
-			{
-				if (ModulesManager.isModuleEnabled('SecuritySettingsWebclient'))
-				{
-					ModulesManager.run(
-						'SecuritySettingsWebclient',
-						'registerSecuritySettingsSection', 
-						[
-							function () { return require('modules/%ModuleName%/js/views/TwoFactorAuthSettingsFormView.js'); },
-							'%ModuleName%'
-						]
-					);
-				}
-				else
-				{
-					ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [
-						function () { return require('modules/%ModuleName%/js/views/TwoFactorAuthSettingsFormView.js'); },
-						Settings.HashModuleName,
-						TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')
-					]);
-				}
-			}
+  return {
+    /**
+     * Runs before application start. Subscribes to the event before post displaying.
+     *
+     * @param {Object} ModulesManager
+     */
+    start: function (ModulesManager) {
+      if (!App.isMobile()) {
+        if (ModulesManager.isModuleEnabled('SecuritySettingsWebclient')) {
+          ModulesManager.run('SecuritySettingsWebclient', 'registerSecuritySettingsSection', [
+            function () {
+              return require('modules/%ModuleName%/js/views/TwoFactorAuthSettingsFormView.js')
+            },
+            '%ModuleName%',
+          ])
+        } else {
+          ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [
+            function () {
+              return require('modules/%ModuleName%/js/views/TwoFactorAuthSettingsFormView.js')
+            },
+            Settings.HashModuleName,
+            TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB'),
+          ])
+        }
+      }
 
-			if (App.getUserRole() === Enums.UserRole.Anonymous)
-			{
-				var onAfterlLoginFormConstructView = function (oParams) {
-					var
-						oLoginScreenView = oParams.View,
-						Popups = require('%PathToCoreWebclientModule%/js/Popups.js'),
-						VerifySecondFactorPopup = require('modules/%ModuleName%/js/popups/VerifySecondFactorPopup.js')
-					;
+      if (App.getUserRole() === Enums.UserRole.Anonymous) {
+        var onAfterlLoginFormConstructView = function (oParams) {
+          var oLoginScreenView = oParams.View,
+            Popups = require('%PathToCoreWebclientModule%/js/Popups.js'),
+            VerifySecondFactorPopup = require('modules/%ModuleName%/js/popups/VerifySecondFactorPopup.js')
+          if (oLoginScreenView) {
+            // Do not completely replace previous onSystemLoginResponse, because it might be already changed by another plugin
+            var fOldOnSystemLoginResponse = oLoginScreenView.onSystemLoginResponse.bind(oLoginScreenView)
+            if (!_.isFunction(fOldOnSystemLoginResponse)) {
+              fOldOnSystemLoginResponse = oLoginScreenView.onSystemLoginResponseBase.bind(oLoginScreenView)
+            }
+            if (!_.isFunction(fOldOnSystemLoginResponse)) {
+              fOldOnSystemLoginResponse = function () {}
+            }
+            oLoginScreenView.onSystemLoginResponse = function (oResponse, oRequest) {
+              if (oRequest.Parameters.Domain !== undefined) {
+                oRequest.Parameters.Login = oRequest.Parameters.Login + '@' + oRequest.Parameters.Domain
+              }
 
-					if (oLoginScreenView)
-					{
-						// Do not completely replace previous onSystemLoginResponse, because it might be already changed by another plugin
-						var fOldOnSystemLoginResponse = oLoginScreenView.onSystemLoginResponse.bind(oLoginScreenView);
-						if (!_.isFunction(fOldOnSystemLoginResponse))
-						{
-							fOldOnSystemLoginResponse = oLoginScreenView.onSystemLoginResponseBase.bind(oLoginScreenView);
-						}
-						if (!_.isFunction(fOldOnSystemLoginResponse))
-						{
-							fOldOnSystemLoginResponse = function () {};
-						}
-						oLoginScreenView.onSystemLoginResponse = function (oResponse, oRequest) {
+              //if TwoFactorAuth enabled - trying to verify user token
+              var oTwoFactorAuthData = oResponse.Result && oResponse.Result.TwoFactorAuth
+              if (oTwoFactorAuthData) {
+                Popups.showPopup(VerifySecondFactorPopup, [
+                  _.bind(this.onSystemLoginResponseBase, this),
+                  _.bind(function () {
+                    this.loading(false)
+                  }, this),
+                  oTwoFactorAuthData,
+                  oRequest.Parameters.Login,
+                  oRequest.Parameters.Password,
+                ])
+              } else {
+                fOldOnSystemLoginResponse(oResponse, oRequest)
+              }
+            }
+          }
+        }.bind(this)
+        App.subscribeEvent('StandardLoginFormWebclient::ConstructView::after', onAfterlLoginFormConstructView)
+        App.subscribeEvent('MailLoginFormWebclient::ConstructView::after', onAfterlLoginFormConstructView)
+      }
 
-							if (oRequest.Parameters.Domain !== undefined)
-							{
-								oRequest.Parameters.Login = oRequest.Parameters.Login + '@' + oRequest.Parameters.Domain;
-							}
-
-							//if TwoFactorAuth enabled - trying to verify user token
-							var oTwoFactorAuthData = oResponse.Result && oResponse.Result.TwoFactorAuth;
-							if (oTwoFactorAuthData)
-							{
-								Popups.showPopup(VerifySecondFactorPopup, [
-									_.bind(this.onSystemLoginResponseBase, this),
-									_.bind(function () { this.loading(false); }, this),
-									oTwoFactorAuthData,
-									oRequest.Parameters.Login,
-									oRequest.Parameters.Password
-								]);
-							}
-							else
-							{
-								fOldOnSystemLoginResponse(oResponse, oRequest);
-							}
-						};
-					}
-				}.bind(this);
-				App.subscribeEvent('StandardLoginFormWebclient::ConstructView::after', onAfterlLoginFormConstructView);
-				App.subscribeEvent('MailLoginFormWebclient::ConstructView::after', onAfterlLoginFormConstructView);
-			}
-			
-			if (App.isUserNormalOrTenant())
-			{
-				var oParameters = {
-					'DeviceId': App.getCurrentDeviceId(),
-					'DeviceName': DeviceUtils.getName()
-				};
-				Ajax.send('%ModuleName%', 'SaveDevice', oParameters);
-			}
-		}
-	};
-};
+      if (App.isUserNormalOrTenant()) {
+        var oParameters = {
+          DeviceId: App.getCurrentDeviceId(),
+          DeviceName: DeviceUtils.getName(),
+        }
+        Ajax.send('%ModuleName%', 'SaveDevice', oParameters)
+      }
+    },
+  }
+}
